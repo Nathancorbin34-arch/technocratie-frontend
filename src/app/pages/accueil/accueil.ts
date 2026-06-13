56f64c345756adb565a61c1f148f0ba2bba08133
Corbin <nathan.corbin34@gmail.com>
2026-06-13 19:40:15 +0200

    Slider avec animation + swipe mobile

--- src/app/pages/accueil/accueil.ts
+++ src/app/pages/accueil/accueil.ts
@@ -55,15 +55,47 @@ export class Accueil {
     }
   ];
 
-tailles = ['XS', 'S', 'M', 'L', 'XL'];
+  tailles = ['XS', 'S', 'M', 'L', 'XL'];
   activeImages = [0, 0, 0, 0];
+  touchStartX = 0;
+  touchEndX = 0;
 
-  nextImage(index: number) {
-    this.activeImages[index] = this.activeImages[index] === 0 ? 1 : 0;
+nextImage(index: number) {
+  this.activeImages[index] = this.activeImages[index] === 0 ? 1 : 0;
+  this.updateSlider(index);
+}
+
+prevImage(index: number) {
+  this.activeImages[index] = this.activeImages[index] === 0 ? 1 : 0;
+  this.updateSlider(index);
+}
+
+updateSlider(index: number) {
+  const sliders = document.querySelectorAll('.product-image-track');
+  const slider = sliders[index] as HTMLElement;
+  if (slider) {
+    if (this.activeImages[index] === 1) {
+      slider.classList.add('show-back');
+    } else {
+      slider.classList.remove('show-back');
+    }
+  }
+}
+
+  onTouchStart(event: TouchEvent, index: number) {
+    this.touchStartX = event.touches[0].clientX;
   }
 
-  prevImage(index: number) {
-    this.activeImages[index] = this.activeImages[index] === 0 ? 1 : 0;
+  onTouchEnd(event: TouchEvent, index: number) {
+    this.touchEndX = event.changedTouches[0].clientX;
+    const diff = this.touchStartX - this.touchEndX;
+    if (Math.abs(diff) > 50) {
+      if (diff > 0) {
+        this.nextImage(index);
+      } else {
+        this.prevImage(index);
+      }
+    }
   }
 
   selectionnerTaille(productIndex: number, taille: string) {
