c6cbc6ae725a92c1f14b987ce52976641507f289
Corbin <nathan.corbin34@gmail.com>
2026-06-14 15:34:22 +0200

    fix panier temps reel + navbar

--- src/app/composants/navbar/navbar.ts
+++ src/app/composants/navbar/navbar.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { PanierService } from '../../services/panier';
 import { AuthService } from '../../services/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterLink, RouterLinkActive, CommonModule, AsyncPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar implements OnInit {
 
   client: any = null;
+  nombreItems$!: Observable<number>;
 
   constructor(
     public panierService: PanierService,
@@ -24,6 +27,9 @@ export class Navbar implements OnInit {
     this.authService.client$.subscribe(client => {
       this.client = client;
     });
+    this.nombreItems$ = this.panierService.panier$.pipe(
+      map(items => items.reduce((total, i) => total + i.quantite, 0))
+    );
   }
 
   seDeconnecter() {
